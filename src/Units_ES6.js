01bde50a51011a4acff208fed553fcf457f88697
Kamila Zaeva <Kamila_Zaeva@epam.com>
2016-05-20 16:54:19 +0300

    complete task

--- src/Units_ES6.js
+++ src/Units_ES6.js
@@ -1,16 +1,16 @@
 class Unit {
 	constructor (strength) {
-    this.strength  = strength; // strength - количество боевых единиц
-  }
-  logStrength () {
-  	console.log("Strength: " + this.strength);
-  }
-  add (unit) {
-  	this.strength  += parseInt(unit);
-  }
-  countStrength() {
-  	return this.strength;
-  }
+		this.strength  = strength; // strength - количество боевых единиц
+	}
+	logStrength () {
+		console.log("Strength: " + this.strength);
+	}
+	add (unit) {
+		this.strength  += parseInt(unit);
+	}
+	countStrength() {
+		return this.strength;
+	}
 }
 
 // взвод - из боевых единиц
